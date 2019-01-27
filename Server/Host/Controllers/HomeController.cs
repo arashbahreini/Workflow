791b076748f3b54dd4b612be217cb7775e0910c4
arash bahreini <vbhost.ir@gmail.com>
2019-01-27 10:30:47 +0330

    Added workflows and run workflow engine from the host project.

--- Server/Host/Controllers/HomeController.cs
+++ Server/Host/Controllers/HomeController.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Host.Models;
+using Microsoft.Extensions.Options;
+using workflow.Core;
 
 namespace Host.Controllers
 {
     public class HomeController : Controller
     {
+        private static IOptions<Configuration> _config;
+        public HomeController(IOptions<Configuration> config)
+        {
+            _config = config;
+        }
         public IActionResult Index()
         {
+            new workflowEngine(_config.Value.WorkflowSettingsFile, true).Run();
             return View();
         }
 
--- Server/Host/Program.cs
+++ Server/Host/Program.cs
@@ -7,11 +7,14 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using workflow.Core;
 
 namespace Host
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
